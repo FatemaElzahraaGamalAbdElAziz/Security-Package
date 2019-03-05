7da0494273d5a3237265cb2a851fc249039300fd
fatmaOsama <fatma.osama@gmail.com>
2019-03-05 18:11:19 +0200

    Add RailFence Ecryption and Decryption

--- SecurityPackage/securitylibrary/MainAlgorithms/RailFence.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/RailFence.cs
@@ -13,14 +13,61 @@ namespace SecurityLibrary
             throw new NotImplementedException();
         }
 
-        public string Decrypt(string cipherText, int key)
+        public string Decrypt(string CipherText, int key)
         {
-            throw new NotImplementedException();
+            int Depth = (int)Math.Ceiling((double)CipherText.Length / key);
+            char[,] Matrix = new char[key, Depth];
+            int index = 0;
+            for (int i = 0; i < key; i++)
+            {
+                for (int j = 0; j < Depth; j++)
+                {
+                    Matrix[i, j] = CipherText[index];
+                    index++;
+                }
+            }
+            string Result = "";
+            for (int i = 0; i < Depth; i++)
+            {
+                for (int j = 0; j < key; j++)
+                {
+                    Result += (Matrix[j, i]);
+                }
+            }
+            return Result;
         }
 
         public string Encrypt(string plainText, int key)
         {
-            throw new NotImplementedException();
+            int Depth = (int)Math.Ceiling((double)plainText.Length / key);
+            char[,] Matrix = new char[key, Depth];
+            int index = 0;
+            for (int i = 0; i < Depth; i++)
+            {
+                for (int j = 0; j < key; j++)
+                {
+                    if (index < plainText.Length)
+                    {
+                        Matrix[j, i] = plainText[index];
+                        index++;
+                    }
+                    else
+                    {
+                        break;
+                        Matrix[j, i] = 'x';
+
+                    }
+                }
+            }
+            string Result = "";
+            for (int i = 0; i < key; i++)
+            {
+                for (int j = 0; j < Depth; j++)
+                {
+                    Result += (Matrix[i, j]);
+                }
+            }
+            return Result;
         }
     }
 }
