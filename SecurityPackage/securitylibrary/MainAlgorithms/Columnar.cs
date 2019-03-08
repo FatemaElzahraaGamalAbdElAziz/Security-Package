1995fc3289dc7e099cfec190c5ca8cb77e4c45bd
fatmaOsama <fatma.osama@gmail.com>
2019-03-08 11:47:44 +0200

    Columner Encryption

--- SecurityPackage/securitylibrary/MainAlgorithms/Columnar.cs
+++ SecurityPackage/securitylibrary/MainAlgorithms/Columnar.cs
@@ -20,7 +20,42 @@ namespace SecurityLibrary
 
         public string Encrypt(string plainText, List<int> key)
         {
-            throw new NotImplementedException();
+            int col = key.Count, row = (int)Math.Ceiling((double)plainText.Length / key.Count);
+            char[,] Matrix = new char[row, col], NewMatrix = new char[row, col];
+            int index = 0;
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    if (index < plainText.Length)
+                    {
+
+                        Matrix[i, j] = plainText[index];
+                        index++;
+                    }
+                    else
+                    {
+                        Matrix[i, j] = 'x';
+                    }
+                }
+            }
+            string Result = "";
+            for (int i = 0; i < row; i++)
+            {
+                for (int j = 0; j < col; j++)
+                {
+                    int t = key.IndexOf(j + 1);
+                    NewMatrix[i, j] = Matrix[i, t];
+                }
+            }
+            for (int j = 0; j < col; j++)
+            {
+                for (int i = 0; i < row; i++)
+                {
+                    Result += NewMatrix[i, j];
+                }
+            }
+            return Result.ToUpper();
         }
     }
 }
