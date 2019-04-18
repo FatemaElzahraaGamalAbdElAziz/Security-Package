d92de8261124e2415f92b7b07c813ac16e122866
fatmaOsama <fatma.osama@gmail.com>
2019-04-18 13:31:21 +0200

    Deffie Hellman Key Exchange

--- SecurityPackage/securitylibrary/DiffieHellman/DiffieHellman.cs
+++ SecurityPackage/securitylibrary/DiffieHellman/DiffieHellman.cs
@@ -8,9 +8,25 @@ namespace SecurityLibrary.DiffieHellman
 {
     public class DiffieHellman 
     {
+      
+        public static int FastPower(int b, int p, int mod)
+        {
+            if (p == 0) return 1;
+            int res = FastPower(b, p / 2, mod);
+            res *= res;
+            res %= mod;
+            if (p % 2 == 1) res = (res * b) % mod;
+            return res;
+        }
         public List<int> GetKeys(int q, int alpha, int xa, int xb)
         {
-            throw new NotImplementedException();
+            List<int> Result = new List<int>();
+            int ya = FastPower(alpha, xa, q);
+            int yb = FastPower(alpha, xb, q);
+            Console.WriteLine(ya + " " + yb);
+            Result.Add( FastPower(yb, xa, q));
+            Result.Add(FastPower(ya, xb, q));
+            return Result;
         }
     }
 }
